37f41b5849b8d5ab40fad6601ce28d31e106a463
Cleiton Araújo <cltn.info@gmail.com>
2023-10-03 21:55:18 -0300

    Criando a view CheckgoutCompleto - Exibindo um resumo do pedido. Alterando também a tabela Pedido

---
+++ Migrations/20231004005221_PedidoDetalheCep.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PedidoDetalheCep : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cep",
+                table: "Pedidos",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CarrinhoCompraId",
+                table: "CarrinhoCompraItens",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cep",
+                table: "Pedidos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CarrinhoCompraId",
+                table: "CarrinhoCompraItens",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
